cdad3a5568779ecab3fa2c96ca1579f99bf31e78
Charlie Calvert <charlie@elvenware.com>
2013-10-20 20:24:46 -0400

    Type fixed

--- NodeTest/app.js
+++ NodeTest/app.js
@@ -1,5 +1,6 @@
 
-git var port = process.env.PORT || 30025;
+var port = process.env.PORT || 30025;
+var http = require("http");
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
