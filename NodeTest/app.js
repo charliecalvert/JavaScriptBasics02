e4f4a09f73ec8cb885b0dff71d2748c34a463aa1
Charlie Calvert <charlie@elvenware.com>
2013-10-20 20:22:44 -0400

    Endings

--- NodeTest/app.js
+++ NodeTest/app.js
@@ -1,5 +1,5 @@
-var http = require('http');
-var port = process.env.PORT || 30025;
+
+git var port = process.env.PORT || 30025;
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
